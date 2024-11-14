c23caab41e47c955da28a8b5921026fbf3b96ece
m7mdragab <mr341349@gmail.com>
2024-11-14 14:37:34 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/views/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
+}
