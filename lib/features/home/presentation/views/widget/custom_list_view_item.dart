a0360033ca828311e583178255e22de05762bf65
m7mdragab <mr341349@gmail.com>
2024-11-19 09:50:08 +0200

    add featured list view to home

--- lib/features/home/presentation/views/widget/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widget/custom_list_view_item.dart
@@ -6,18 +6,15 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.25,
-      child: AspectRatio(
-        aspectRatio: 3.8 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16),
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(AssetsData.test),
-            ),
+    return AspectRatio(
+      aspectRatio: 3.8 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.test),
           ),
         ),
       ),
