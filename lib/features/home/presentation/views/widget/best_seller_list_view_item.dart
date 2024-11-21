2a732cd14432beefe94a953f4a0ade30b6b5e9cc
m7mdragab <mr341349@gmail.com>
2024-11-21 14:49:57 +0200

    refactor code and install font family

---
+++ lib/features/home/presentation/views/widget/best_seller_list_view_item.dart
@@ -0,0 +1,30 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.test),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
