41bbe15d1f4843666ba00ad131884b240e85be10
m7mdragab <mr341349@gmail.com>
2024-11-21 18:33:35 +0200

    best seller item part 3

--- lib/features/home/presentation/views/widget/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widget/best_seller_list_view_item.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -28,16 +29,39 @@ class BestSellerListViewItem extends StatelessWidget {
             width: MediaQuery.of(context).size.width * 1 / 13,
           ),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * 1 / 2,
-                child: const Text(
+                child: Text(
                   'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20,
+                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
                 ),
               ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 1 / 247,
+              ),
+              Text(
+                'J.K.Rowling',
+                style: Styles.textStyle14,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 1 / 247,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '19.99 €',
+                    style: Styles.textStyle20.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ],
