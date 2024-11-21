6b06ffa9fe2753c924ed5613bb90eef196f0851e
m7mdragab <mr341349@gmail.com>
2024-11-21 17:46:18 +0200

    done edit styles file

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const kBackPrimaryColor = Color(0xff100B20);
 const kTransitionDuration = Duration(milliseconds: 250);
-const KGtSectraFine = 'GT Sectra Fine';
+const kGtSectraFine = 'GT Sectra Fine';
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
@@ -6,8 +7,25 @@ abstract class Styles {
     fontWeight: FontWeight.w600,
   );
 
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSectraFine,
+  );
+
   static const textStyle30 = TextStyle(
     fontSize: 30,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.bold,
+    fontFamily: kGtSectraFine,
+  );
+
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
   );
 }
