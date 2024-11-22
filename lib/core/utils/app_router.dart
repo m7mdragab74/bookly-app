fd971d715643c677f2d1592f1b9a585f3b93b54f
m7mdragab <mr341349@gmail.com>
2024-11-22 18:10:29 +0200

    create custom book details app bar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,10 +1,12 @@
+import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
-  static const kHomeView = '/homeView';
+  static const homeView = '/homeView';
+  static const bookDetailsView = '/bookDetailsView';
   static final GoRouter router = GoRouter(
     routes: <RouteBase>[
       GoRoute(
@@ -14,9 +16,15 @@ abstract class AppRouter {
         },
       ),
       GoRoute(
-        path: kHomeView,
+        path: homeView,
         builder: (BuildContext context, GoRouterState state) {
           return const HomeView();
+        },
+      ),
+      GoRoute(
+        path: bookDetailsView,
+        builder: (BuildContext context, GoRouterState state) {
+          return const BookDetailsView();
         },
       ),
     ],
--- lib/features/home/presentation/views/widget/book_details_view_body.dart
+++ lib/features/home/presentation/views/widget/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/presentation/views/widget/custom_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -5,6 +6,13 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: CustomBookDetailsAppBar(),
+        ),
+      ],
+    );
   }
 }
