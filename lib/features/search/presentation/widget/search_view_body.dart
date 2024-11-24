94348fb3ffa60e3c7d2a31ab7e13136393ac5f4f
m7mdragab <mr341349@gmail.com>
2024-11-24 21:03:08 +0200

    search result view ui done

--- lib/features/search/presentation/widget/search_view_body.dart
+++ lib/features/search/presentation/widget/search_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/search/presentation/widget/custom_search_text_field.dart';
+import 'package:bookly_app/features/search/presentation/widget/search_result_list_view.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -9,8 +11,25 @@ class SearchViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
-        children: const [
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.03,
+          ),
           CustomSearchTextField(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.04,
+          ),
+          const Text(
+            'Result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          Expanded(
+            child: SearchResultListView(),
+          ),
         ],
       ),
     );
