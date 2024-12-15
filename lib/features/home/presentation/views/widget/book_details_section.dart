03380b2cfb5d96de3a8bd68eb4b419731344f855
m7mdragab <mr341349@gmail.com>
2024-12-16 00:25:27 +0200

    change model location to core

--- lib/features/home/presentation/views/widget/book_details_section.dart
+++ lib/features/home/presentation/views/widget/book_details_section.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/presentation/views/widget/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widget/books_action.dart';
 import 'package:bookly_app/features/home/presentation/views/widget/custom_book_image.dart';
